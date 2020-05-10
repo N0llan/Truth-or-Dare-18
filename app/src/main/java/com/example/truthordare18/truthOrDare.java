8d6fb46ac0ff31ee119f7715fb01e6cd7b04c38f
Nollan <Nollan@home>
2020-05-10 08:49:06 +0200

    Initial commit

---
+++ app/src/main/java/com/example/truthordare18/truthOrDare.java
@@ -0,0 +1,14 @@
+package com.example.truthordare18;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class truthOrDare extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_truth_or_dare);
+    }
+}
